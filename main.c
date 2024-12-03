981c08f234bcd936d1fb7efea1f16c5debab33ce
Jej0 <76699284+Jej0@users.noreply.github.com>
2024-12-03 23:29:00 +0000

    ajout de l'ancien code de replit dans main

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include<stdlib.h>
+
+
+
+
+typedef struct arbre{
+  int identifiant;
+  int capacité;
+  int consomateur;
+  struct arbre *fils_gauche;
+  struct arbre *fils_droit;
+
+}arbre;
+
+
 
 int main(void) {
-  printf("Hello World test 1 2 c'est flopesque\n");
+  printf("Hello Axel\n");
   return 0;
 }

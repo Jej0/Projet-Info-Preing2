b389d2f95a3c065e83d8dde1c3abc1f7c9ddc53f
adou-hbt <adouhumblot@gmail.com>
2024-12-04 00:35:30 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+typedef struct arbre{
+  int identifiant;
+  int capacité;
+  int consomateur;
+  struct arbre *fils_gauche;
+  struct arbre *fils_droit;
+}arbre;
 
 int main(void) {
   printf("Hello World test 1 2 c'est flopesque\n");

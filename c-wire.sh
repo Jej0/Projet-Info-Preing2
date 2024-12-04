37f8758e05e473a283982c09c2658c6fc431dc48
Jej0 <76699284+Jej0@users.noreply.github.com>
2024-12-04 14:36:28 +0000

    message d'aide

--- c-wire.sh
+++ c-wire.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
 
+function afficher_aide {
+
+    echo "Utilisation : $0 [-h | --help] <path> <hvb | hva | lv> <comp | indiv | all> [<value>]"
+    echo
+    echo "Options:"
+    echo
+    echo "(optionnel) [-h | --help] : Affiche le menu d'aide"
+    echo
+    echo "(obligatoire) <path> : Chemin vers le fichier de donnée"
+    echo
+    echo "(obligatoire) <hvb | hva | lv> : Le type de station séléctionnée ('hvb' : station haut voltage, 'hva' : station moyen  voltage, 'lv' : station bas voltage)"
+    echo
+    echo "(obligatoire) <comp | indiv | all> : Le type de client séléctionné ('comp' : les entreprises, 'indiv' : les particuliers, 'all' : tout le monde)"
+    echo 
+    echo "(optionnel) [<value>] : Valeur positive (>0) pour séléctionner une centrale spécifique"
+}
+
+for arg in "$@"; do
+    if [[ "$arg" == "-h" || "$arg" == "--help" ]]; then
+        afficher_aide
+        exit 0
+    fi
+done
+
+
 if [ $# -lt 3 ]; then
     echo "Erreur : Vous devez fournir au moins 3 arguments."
     exit 1
@@ -27,3 +52,16 @@ if [[ ("$2" = "hvb" && "$3" = "all") || ("$2" = "hvb" && "$3" = "indiv") || ("$2
     echo "Les stations hvb et hva ne sont pas reliées aux particuliers"
     exit 1
 fi
+
+if [ -n "$4" ]; then
+    if [[ "$4" -gt 0 ]]; then
+        echo "arg 4 est positif"
+    else
+        echo "mauvais arg 4"
+        exit 1
+    fi
+else   
+    echo "Il n'y a pas de 4e paramètre"
+fi
+
+

964057588c634fc58021f9e3db4c6c2160bc09cd
Jej0 <76699284+Jej0@users.noreply.github.com>
2024-12-04 13:34:55 +0000

    c-wire

--- c-wire.sh
+++ c-wire.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
 
-if [ $# -eq 0 ]; then
-    echo "Erreur : Vous devez fournir le chemin d'un fichier en paramètre."
+if [ $# -lt 3 ]; then
+    echo "Erreur : Vous devez fournir au moins 3 arguments."
     exit 1
 fi
 
-if [ -e "$1" ]; then
+if [ -f "$1" ]; then
     echo "Le fichier '$1' existe"
 else
     echo "Le fichier '$1' n'existe pas"
+    exit 1
+fi
+
+if [[ "$2" != "hvb" && "$2" != "hva" && "$2" != "lv" ]]; then
+    echo "Le 2eme paramètre doit être 'hvb', 'hva ou 'lv'."
+    exit 1
+fi
+
+if [[ "$3" != "comp" && "$3" != "indiv" && "$3" != "all" ]]; then
+    echo "Le 3eme paramètre doit être 'comp', 'indv', 'all'."
+    exit 1
+fi
+
+if [[ ("$2" = "hvb" && "$3" = "all") || () ]]; then
+    echo "Le 3eme paramètre doit être différent de 'hvb all', 'hvb indv', 'hva all', 'hva indiv'."
+    exit 1
 fi

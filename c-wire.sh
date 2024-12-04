599352d1fc65a8f65f59050b8e936c3e50002f0f
Jej0 <76699284+Jej0@users.noreply.github.com>
2024-12-04 13:00:55 +0000

    ajout de condition de chemin a c-wire

--- c-wire.sh
+++ c-wire.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 
-if [ -z "$1" ]; then
-    echo "Erreur : Vous devez fournir le chemin d'un fichier .csv en paramètre."
+if [ $# -eq 0 ]; then
+    echo "Erreur : Vous devez fournir le chemin d'un fichier en paramètre."
     exit 1
+fi
+
+if [ -e "$1" ]; then
+    echo "Le fichier '$1' existe"
+else
+    echo "Le fichier '$1' n'existe pas"
 fi
